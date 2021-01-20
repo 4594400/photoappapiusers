bca3bda15339caa4fbb17f527c293bd38321e8c0
anDrekon <4594400@gmail.com>
2021-01-20 12:39:11 +0200

    Initial commit

---
+++ src/test/java/com/doc/photoapp/api/users/PhotoappapiusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doc.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapiusersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
